0f252f40c5a988771218bfd1fca7d3d354b924bf
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-06-14 17:57:05 +0200

    Projektdateien hinzufügen.

---
+++ SchlumpfSoftControls/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+Imports System.Resources
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic
+
+<Assembly: AssemblyTitle("SchlumpfSoftControls")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Andreas Sauer")>
+<Assembly: AssemblyProduct("SchlumpfSoftControls")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("SchlumpfSoft")>
+<Assembly: AssemblyCulture("")>
+<Assembly: ComVisible(False)>
+<Assembly: AssemblyVersion("1.2026.0614.0")>
+<Assembly: AssemblyFileVersion("1.2026.0614.0")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
