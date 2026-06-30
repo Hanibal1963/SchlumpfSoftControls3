3cdb649ec8bbc59cbb7dc0f406f9ce213e51b88f
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-06-30 18:47:12 +0200

    Aktualisiere Assembly-Versionen auf 1.2026.0630.0
    Die Assembly-Versionen wurden in mehreren Projekten und zugehörigen `AssemblyInfo.vb`-Dateien aktualisiert:
    
    - `DriveWatcherControl.vbproj`: Version von 1.2026.0626.4 auf 1.2026.0630.0.
    - `DriveWatcherControlTest.vbproj`: Version von 1.2026.0621.0 auf 1.2026.0630.0.
    - `SharedAttributes.vbproj`: Version von 1.2026.0626.2 auf 1.2026.0630.0.
    - In allen zugehörigen `AssemblyInfo.vb`-Dateien wurden `AssemblyVersion` und `AssemblyFileVersion` entsprechend angepasst.
    
    Diese Änderungen sind Teil einer koordinierten Veröffentlichung der neuen Version 1.2026.0630.0.

--- SharedAttributes/My Project/AssemblyInfo.vb
+++ SharedAttributes/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("1.2026.0626.2")>
-<Assembly: AssemblyFileVersion("1.2026.0626.2")>
+<Assembly: AssemblyVersion("1.2026.0630.0")>
+<Assembly: AssemblyFileVersion("1.2026.0630.0")>
