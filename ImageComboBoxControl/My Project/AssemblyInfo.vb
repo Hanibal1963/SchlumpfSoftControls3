c7d5a0e9a0f6b81d2f3e8419b39f9b50e559bde0
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-07-05 20:14:36 +0200

    Aktualisiere Assembly-Versionen
    Die Datei `ImageComboBoxControl.vbproj` wurde aktualisiert, um die neue Eigenschaft `<AssemblyVersion>` mit dem Wert `1.2026.0705.0` hinzuzufügen. In der Datei `AssemblyInfo.vb` wurden die Attribute `<Assembly: System.Reflection.AssemblyVersion>` und `<Assembly: System.Reflection.AssemblyFileVersion>` von `1.0.0.0` auf `1.2026.0705.0` geändert, um die Versionsnummern zu synchronisieren. Ein unsichtbares Zeichen im Kommentarblock wurde ebenfalls entfernt.

--- ImageComboBoxControl/My Project/AssemblyInfo.vb
+++ ImageComboBoxControl/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿' --------------------------------------------------------------------------------------------------------
+' --------------------------------------------------------------------------------------------------------
 ' Datei: AssemblyInfo.vb
 ' Author: Andreas Sauer
 ' Datum: 05.07.2026
@@ -11,6 +11,6 @@
 <Assembly: System.Reflection.AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
 <Assembly: System.Reflection.AssemblyTrademark("SchlumpfSoft")>
 <Assembly: System.Runtime.InteropServices.ComVisible(False)>
-<Assembly: System.Reflection.AssemblyVersion("1.0.0.0")>
-<Assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")>
+<Assembly: System.Reflection.AssemblyVersion("1.2026.0705.0")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.2026.0705.0")>
 <Assembly: System.Resources.NeutralResourcesLanguage("de-DE")>
