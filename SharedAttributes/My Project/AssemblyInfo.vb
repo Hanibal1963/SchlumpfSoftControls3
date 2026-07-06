9c032789a745f0269aa356b31e98abc9cf1a20e8
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-07-06 20:36:52 +0200

    Verwende vollständige Namespace-Qualifikationen
    In der Datei `AssemblyInfo.vb` wurden die Import-Anweisungen für die Namespaces `System`, `System.Reflection` und `System.Runtime.InteropServices` entfernt. Stattdessen werden nun die vollständigen Qualifikationen für die Assembly-Attribute verwendet. Ein Kommentarblock mit Metadaten zur Datei wurde hinzugefügt. Die Versionsnummern der Assembly wurden von `1.2026.0706.0` auf `1.2026.0706.1` erhöht.

--- SharedAttributes/My Project/AssemblyInfo.vb
+++ SharedAttributes/My Project/AssemblyInfo.vb
@@ -1,32 +1,16 @@
-Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+' --------------------------------------------------------------------------------------------------------
+' Datei: AssemblyInfo.vb
+' Author: Andreas Sauer
+' Datum: 06.07.2026
+' --------------------------------------------------------------------------------------------------------
 
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
-
-' Werte der Assemblyattribute überprüfen
-
-<Assembly: AssemblyTitle("SharedAttributes")>
-<Assembly: AssemblyDescription("Bibliothek mit Attributdefinitionen")>
-<Assembly: AssemblyCompany("Andreas Sauer")>
-<Assembly: AssemblyProduct("SharedAttributes")>
-<Assembly: AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
-<Assembly: AssemblyTrademark("SchlumpfSoft")>
-
-<Assembly: ComVisible(False)>
-
-'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
-<Assembly: Guid("1d94ad8a-5dbb-48d4-b128-18247638cf10")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-
-<Assembly: AssemblyVersion("1.2026.0706.0")>
-<Assembly: AssemblyFileVersion("1.2026.0706.0")>
+<Assembly: System.Reflection.AssemblyTitle("SharedAttributes")>
+<Assembly: System.Reflection.AssemblyDescription("Bibliothek mit Attributdefinitionen")>
+<Assembly: System.Reflection.AssemblyCompany("Andreas Sauer")>
+<Assembly: System.Reflection.AssemblyProduct("SharedAttributes")>
+<Assembly: System.Reflection.AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
+<Assembly: System.Reflection.AssemblyTrademark("SchlumpfSoft")>
+<Assembly: System.Runtime.InteropServices.ComVisible(False)>
+<Assembly: System.Runtime.InteropServices.Guid("1d94ad8a-5dbb-48d4-b128-18247638cf10")>
+<Assembly: System.Reflection.AssemblyVersion("1.2026.0706.1")>
+<Assembly: System.Reflection.AssemblyFileVersion("1.2026.0706.1")>
