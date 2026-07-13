062ebd3278f3fba2b784cdfa581a79137fd833b0
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-07-13 17:59:27 +0200

    Neue Steuerelemente und Ereignishandler hinzugefügt
    In `FormMain.Designer.vb` wurden ein `TextBox`- und ein
    `Button`-Steuerelement hinzugefügt und konfiguriert.
    Eigenschaften der `ImageComboBox1` wurden angepasst, und
    die Formulargröße wurde geändert.
    
    In `FormMain.vb` wurde der Konstruktor erweitert, um den
    Fenstertitel dynamisch zu setzen. Neue Ereignishandler für
    `Button1` und `ImageComboBox1` wurden implementiert,
    einschließlich `Button1_Click`, `ImageComboBox1_SelectedIndexChanged`,
    `ImageComboBox1_TextChanged` und der Hilfsmethode `PrintMessage`.
    
    In `FormMain.resx` wurden Ressourcen für die
    `ImageComboBox1`-Elemente hinzugefügt.
    
    Die Assembly-Version und Datei-Version in `AssemblyInfo.vb`
    wurden auf `1.2026.0713.3` aktualisiert.

--- ImageComboBoxControlTest/FormMain.Designer.vb
+++ ImageComboBoxControlTest/FormMain.Designer.vb
@@ -24,29 +24,52 @@ Partial Class FormMain
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormMain))
         Me.ImageComboBox1 = New SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ImageComboBox1
         '
         Me.ImageComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable
         Me.ImageComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ImageComboBox1.DropDownWidth = 163
+        Me.ImageComboBox1.DropDownWidth = 121
         Me.ImageComboBox1.Elements.Add(CType(resources.GetObject("ImageComboBox1.Elements"), SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBoxItem))
         Me.ImageComboBox1.Elements.Add(CType(resources.GetObject("ImageComboBox1.Elements1"), SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBoxItem))
         Me.ImageComboBox1.Elements.Add(CType(resources.GetObject("ImageComboBox1.Elements2"), SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBoxItem))
         Me.ImageComboBox1.Elements.Add(CType(resources.GetObject("ImageComboBox1.Elements3"), SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBoxItem))
         Me.ImageComboBox1.Elements.Add(CType(resources.GetObject("ImageComboBox1.Elements4"), SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBoxItem))
         Me.ImageComboBox1.FormattingEnabled = True
-        Me.ImageComboBox1.Location = New System.Drawing.Point(30, 24)
+        Me.ImageComboBox1.Location = New System.Drawing.Point(21, 12)
         Me.ImageComboBox1.Name = "ImageComboBox1"
         Me.ImageComboBox1.Size = New System.Drawing.Size(163, 21)
         Me.ImageComboBox1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(21, 39)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(460, 217)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(305, 275)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(176, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Meldungen löschen"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'FormMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(493, 268)
+        Me.ClientSize = New System.Drawing.Size(493, 317)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ImageComboBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
@@ -55,8 +78,11 @@ Partial Class FormMain
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FormMain"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Private WithEvents ImageComboBox1 As SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBox
+    Private WithEvents TextBox1 As TextBox
+    Private WithEvents Button1 As Button
 End Class
--- ImageComboBoxControlTest/FormMain.vb
+++ ImageComboBoxControlTest/FormMain.vb
@@ -7,10 +7,33 @@
 Public Class FormMain
 
     Public Sub New()
+
         ' Dieser Aufruf ist für den Designer erforderlich.
         Me.InitializeComponent()
+
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
         Me.Text = $"{My.Application.Info.AssemblyName} V{My.Application.Info.Version} {My.Application.Info.Copyright}"
+        Me.ImageComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.TextBox1.Clear()
+    End Sub
+
+    Private Sub ImageComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ImageComboBox1.SelectedIndexChanged
+        Dim index As Int32 = CType(sender, SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBox).SelectedIndex
+        Dim message As String = $"SelectedIndexChanged meldet: der Index hat sich zu ""{index}"" geändert."
+        Me.PrintMessage(message)
+    End Sub
+
+    Private Sub ImageComboBox1_TextChanged(sender As Object, e As EventArgs) Handles ImageComboBox1.TextChanged
+        Dim text As String = CType(sender, SchlumpfSoft.Controls.ImageComboBoxControl.ImageComboBox).Text
+        Dim message As String = $"TextChanged meldet: Der Text hat sich zu ""{text}"" geändert."
+        Me.PrintMessage(message)
+    End Sub
+
+    Private Sub PrintMessage(message As String)
+        Me.TextBox1.AppendText($"{message}{Environment.NewLine}")
     End Sub
 
 End Class
