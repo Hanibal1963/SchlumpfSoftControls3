cb98c6896b7146f96b1cd0ef107d235f192637ae
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-07-19 17:57:08 +0200

    Designer-Kommentare in ImageComboBox übersetzt
    Die Kommentare im Designer-Code der ImageComboBox wurden ins Deutsche übersetzt und an die Team-Konventionen angepasst. Es wurden keine funktionalen Änderungen am Code vorgenommen.

--- ImageComboBoxControl/ImageComboBox.Designer.vb
+++ ImageComboBoxControl/ImageComboBox.Designer.vb
@@ -2,8 +2,8 @@
 Partial Class ImageComboBox
     Inherits System.Windows.Forms.UserControl
 
-    'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    ' UserControl überschreibt Dispose, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -14,13 +14,13 @@ Partial Class ImageComboBox
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
+    ' Erforderlich für den Windows-Form-Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    ' NOTE: Die folgende Prozedur ist für den Windows-Form-Designer erforderlich.
+    ' Sie kann mit dem Windows-Form-Designer geändert werden.  
+    ' Nicht mit dem Code-Editor ändern.
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.SuspendLayout()
         '
