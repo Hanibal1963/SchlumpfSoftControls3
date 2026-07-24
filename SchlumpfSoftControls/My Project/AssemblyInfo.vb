77fc2771610ea3de920ca32b13183addb079a5fa
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-07-24 18:27:54 +0200

    Assembly-Version auf 3.2026.0724.0 erhöht
    Die Assembly-Version und AssemblyFileVersion wurden in der AssemblyInfo.vb von 3.2026.0719.1 auf 3.2026.0724.0 angehoben. Dies dient der Kennzeichnung einer neuen Produktversion. Weitere Änderungen wurden nicht vorgenommen.

--- SchlumpfSoftControls/My Project/AssemblyInfo.vb
+++ SchlumpfSoftControls/My Project/AssemblyInfo.vb
@@ -11,6 +11,6 @@
 <Assembly: System.Reflection.AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
 <Assembly: System.Reflection.AssemblyTrademark("SchlumpfSoft")>
 <Assembly: System.Runtime.InteropServices.ComVisible(False)>
-<Assembly: System.Reflection.AssemblyVersion("3.2026.0719.1")>
-<Assembly: System.Reflection.AssemblyFileVersion("3.2026.0719.1")>
+<Assembly: System.Reflection.AssemblyVersion("3.2026.0724.0")>
+<Assembly: System.Reflection.AssemblyFileVersion("3.2026.0724.0")>
 <Assembly: System.Resources.NeutralResourcesLanguage("de-DE")>
