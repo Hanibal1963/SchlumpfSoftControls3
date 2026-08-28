9db91432e8b7daece38c8a5ef6dfef5347fe3aca
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-08-28 13:18:10 +0200

    Code überarbeitet und optimiert, Keine funktionellen Änderungen

--- SchlumpfSoftControls/TransparentLabelControl/TransparentLabel.vb
+++ SchlumpfSoftControls/TransparentLabelControl/TransparentLabel.vb
@@ -4,6 +4,11 @@
 ' Datum: 05.05.2026
 ' --------------------------------------------------------------------------------------------------------
 
+Imports System
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+
 Namespace TransparentLabelControl
 
     ''' <summary>
@@ -12,15 +17,13 @@ Namespace TransparentLabelControl
     <ProvideToolboxControl("SchlumpfSoft Controls", False)>
     <Description("Ein Steuerelement zum Anzeigen eines Textes mit durchscheinendem Hintergrund.")>
     <ToolboxItem(True)>
-    <System.Drawing.ToolboxBitmap(GetType(TransparentLabel), "TransparentLabelControl.TransparentLabel.bmp")>
-    Public Class TransparentLabel
-
-        Inherits System.Windows.Forms.Label
+    <ToolboxBitmap(GetType(TransparentLabel), "TransparentLabelControl.TransparentLabel.bmp")>
+    Public Class TransparentLabel : Inherits Label
 
         ' Konstante für den erweiterten Fensterstil WS_EX_TRANSPARENT, um die Transparenz zu ermöglichen.
-        Private Const WS_EX_TRANSPARENT As System.Int32 = &H20
+        Private Const WS_EX_TRANSPARENT As Int32 = &H20
 
-#Region "Definition der öffentlichen Methoden"
+#Region "Öffentliche Methoden"
 
         ''' <summary>
         ''' Initialisiert eine neue Instanz der <see cref="TransparentLabel"/> -Klasse.
@@ -31,32 +34,32 @@ Namespace TransparentLabelControl
             ' damit Transparenz und Zeichenverhalten korrekt konfiguriert sind.
             Me.Name = NameOf(TransparentLabel)
             Me.InitializeStyles()
-            Me.BackColor = System.Drawing.Color.Transparent
+            Me.BackColor = Color.Transparent
 
         End Sub
 
 #End Region
 
-#Region "Definition der Eigenschaften"
+#Region "Eigenschaften"
 
         ''' <summary>
         ''' Ausgeblendet da für dieses Control nicht relevant.
         ''' </summary>
         <Browsable(False)>
         <EditorBrowsable(EditorBrowsableState.Never)>
-        <DefaultValue(GetType(System.Drawing.Color), "Transparent")>
+        <DefaultValue(GetType(Color), "Transparent")>
         <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
-        Public Overrides Property BackColor As System.Drawing.Color
+        Public Overrides Property BackColor As Color
             Get
                 Return MyBase.BackColor
             End Get
-            Set(value As System.Drawing.Color)
-                If value <> System.Drawing.Color.Transparent AndAlso value <> System.Drawing.Color.Empty Then
-                    Throw New System.ArgumentException("Für dieses Steuerelement ist nur Color.Transparent zulässig.", NameOf(value))
+            Set(value As Color)
+                If value <> Color.Transparent AndAlso value <> Color.Empty Then
+                    Throw New ArgumentException("Für dieses Steuerelement ist nur Color.Transparent zulässig.", NameOf(value))
                 End If
 
                 ' Für dieses Steuerelement wird BackColor immer transparent erzwungen.
-                MyBase.BackColor = System.Drawing.Color.Transparent
+                MyBase.BackColor = Color.Transparent
             End Set
         End Property
 
@@ -66,11 +69,11 @@ Namespace TransparentLabelControl
         <Browsable(False)>
         <EditorBrowsable(EditorBrowsableState.Never)>
         <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
-        Public Overrides Property BackgroundImage As System.Drawing.Image
+        Public Overrides Property BackgroundImage As Image
             Get
                 Return MyBase.BackgroundImage
             End Get
-            Set(value As System.Drawing.Image)
+            Set(value As Image)
                 MyBase.BackgroundImage = value
             End Set
         End Property
@@ -81,11 +84,11 @@ Namespace TransparentLabelControl
         <Browsable(False)>
         <EditorBrowsable(EditorBrowsableState.Never)>
         <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
-        Public Overrides Property BackgroundImageLayout As System.Windows.Forms.ImageLayout
+        Public Overrides Property BackgroundImageLayout As ImageLayout
             Get
                 Return MyBase.BackgroundImageLayout
             End Get
-            Set(value As System.Windows.Forms.ImageLayout)
+            Set(value As ImageLayout)
                 MyBase.BackgroundImageLayout = value
             End Set
         End Property
@@ -96,37 +99,38 @@ Namespace TransparentLabelControl
         <Browsable(False)>
         <EditorBrowsable(EditorBrowsableState.Never)>
         <DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
-        Public Shadows Property FlatStyle As System.Windows.Forms.FlatStyle
+        Public Shadows Property FlatStyle As FlatStyle
             Get
                 Return MyBase.FlatStyle
             End Get
-            Set(value As System.Windows.Forms.FlatStyle)
+            Set(value As FlatStyle)
                 MyBase.FlatStyle = value
             End Set
         End Property
 
 #End Region
 
-#Region "Definition der internen Methoden"
+#Region "Interne Methoden"
 
         ''' <summary>
-        ''' Konfiguriert die relevanten <see cref="System.Windows.Forms.ControlStyles"/> für das Label.
+        ''' Konfiguriert die relevanten <see cref="ControlStyles"/> für das Label.
         ''' </summary>
         ''' <remarks>
-        ''' <para> Die Kombination der Styles sorgt dafür, dass das Steuerelement seinen Hintergrund nicht selbst opak
-        ''' zeichnet und transparente Hintergründe unterstützt. </para> <para> Das Deaktivieren von <c>
-        ''' OptimizedDoubleBuffer</c> hilft in diesem Szenario, unerwünschte Übermal-Effekte bei transparenter Darstellung
-        ''' zu vermeiden. </para>
+        '''
+        ''' <para>Die Kombination der Styles sorgt dafür, dass das Steuerelement seinen Hintergrund nicht selbst opak
+        ''' zeichnet und transparente Hintergründe unterstützt. </para>
+        '''
+        ''' <para>Das Deaktivieren von <c>OptimizedDoubleBuffer</c> hilft in diesem Szenario, unerwünschte
+        ''' Übermal-Effekte bei transparenter Darstellung zu vermeiden. </para>
         ''' </remarks>
         Private Sub InitializeStyles()
 
             ' Relevante Transparenz-Styles in einem Schritt setzen.
-            Me.SetStyle(System.Windows.Forms.ControlStyles.Opaque Or
-                        System.Windows.Forms.ControlStyles.SupportsTransparentBackColor, True)
+            Me.SetStyle(ControlStyles.Opaque Or ControlStyles.SupportsTransparentBackColor, True)
 
             ' Doppelbuffering hier bewusst deaktivieren, damit die Transparenzdarstellung
             ' konsistent mit dem Eltern-Steuerelement funktioniert.
-            Me.SetStyle(System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer, False)
+            Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, False)
 
             ' Style-Änderungen auf das Handle anwenden.
             Me.UpdateStyles()
@@ -137,8 +141,11 @@ Namespace TransparentLabelControl
         ''' Liefert angepasste Erstellungsparameter für das Fensterhandle des Steuerelements.
         ''' </summary>
         ''' <remarks>
-        ''' <para> Das Setzen von <c> WS_EX_TRANSPARENT</c> sorgt dafür, dass zuerst das Eltern-Steuerelement gezeichnet
-        ''' wird und der Hintergrund dadurch durchscheinen kann. </para> <para> <b> Weitere Infos unter:</b><br/>
+        '''
+        ''' <para>Das Setzen von <c>WS_EX_TRANSPARENT</c> sorgt dafür, dass zuerst das Eltern-Steuerelement gezeichnet
+        ''' wird und der Hintergrund dadurch durchscheinen kann. </para>
+        '''
+        ''' <para><b>Weitere Infos unter:</b><br/>
         ''' <see href="https://stackoverflow.com/questions/511320/transparent-control-backgrounds-on-a-vb-net-gradient-filled-form"/>
         ''' <br/> und<br/> <see href="https://learn.microsoft.com/de-de/windows/win32/winmsg/extended-window-styles"/>
         ''' </para>
@@ -146,10 +153,10 @@ Namespace TransparentLabelControl
         ''' <value>
         ''' Die angepassten <see cref="CreateParams"/> mit aktiviertem WS_EX_TRANSPARENT-Stil.
         ''' </value>
-        Protected Overrides ReadOnly Property CreateParams As System.Windows.Forms.CreateParams
+        Protected Overrides ReadOnly Property CreateParams As CreateParams
             Get
                 ' Ausgangswerte vom Basistyp übernehmen.
-                Dim cp As System.Windows.Forms.CreateParams = MyBase.CreateParams
+                Dim cp As CreateParams = MyBase.CreateParams
 
                 ' Erweiterten Fensterstil WS_EX_TRANSPARENT hinzufügen.
                 ' Quelle: https://learn.microsoft.com/de-de/windows/win32/winmsg/extended-window-styles
