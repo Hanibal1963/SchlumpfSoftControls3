4bedab4548f38c032af45314a07ae44fcc612d82
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-06 19:59:52 +0200

    Assembly-Attribute und Kommentare überarbeitet
    Kopfzeile mit Autor, Datum und Dateiname als Kommentar hinzugefügt.
    AssemblyDescription ergänzt und AssemblyCompany auf "Andreas Sauer" gesetzt.
    AssemblyTrademark auf "SchlumpfSoft" geändert.
    NeutralResourcesLanguage("de-DE") hinzugefügt.
    Überflüssige Leerzeilen und Kommentare zu Assembly-Attributen entfernt.
    Imports um System.Resources ergänzt und neu angeordnet.

--- SchlumpfSoftControlsDemo/My Project/AssemblyInfo.vb
+++ SchlumpfSoftControlsDemo/My Project/AssemblyInfo.vb
@@ -1,32 +1,22 @@
-﻿Imports System
+﻿' --------------------------------------------------------------------------------------------------------
+' Datei: AssemblyInfo.vb
+' Author: Andreas Sauer
+' Datum: 06.09.2026
+' --------------------------------------------------------------------------------------------------------
+
+Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
-
-' Werte der Assemblyattribute überprüfen
-
 <Assembly: AssemblyTitle("SchlumpfSoftControlsDemo")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyDescription("Demoprogramm für SchlumpfSoftControls")>
+<Assembly: AssemblyCompany("Andreas Sauer")>
 <Assembly: AssemblyProduct("SchlumpfSoftControlsDemo")>
 <Assembly: AssemblyCopyright("Copyright © 2026 Andreas Sauer")>
-<Assembly: AssemblyTrademark("")>
-
+<Assembly: AssemblyTrademark("SchlumpfSoft")>
 <Assembly: ComVisible(False)>
-
-'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
 <Assembly: Guid("0d4b558b-b3dc-4c8d-b278-fb3d2ae63273")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-
 <Assembly: AssemblyVersion("1.2026.0906.0")>
 <Assembly: AssemblyFileVersion("1.2026.0906.0")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
