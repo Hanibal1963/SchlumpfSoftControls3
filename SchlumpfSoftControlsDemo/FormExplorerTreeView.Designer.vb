e11d1b31c119ef05a2a4560c490480514bf26fdf
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-08 19:40:33 +0200

    Instanznamen und Felder konsolidiert, Stil angepasst
    Die Instanznamen der Steuerelemente wurden konsistent umbenannt (z.B. ExplorerTreeView1 zu ExplorerTreeView, FolderBrowserDialog1 zu FolderBrowserDialog).
    Lokale Variablen für SplitContainer1 und SplitContainer2 wurden entfernt und als Felder der Form deklariert.
    Die Initialisierung und das Hinzufügen der Steuerelemente zu den Panels erfolgt nun über die Membervariablen.
    Event-Handler für SelectedPathChanged und ExpandPath wurden auf die neuen Membernamen angepasst.
    Die Ressourcen-Datei (.resx) enthält nun TrayLocation für FolderBrowserDialog.
    Imports um SchlumpfSoft.Controls erweitert.
    Event-Handler-Zuweisung für ExplorerTreeView.SelectedPathChanged korrigiert.
    Kommentare und Code-Stil entsprechen nun den Namenskonventionen und Vorgaben.

--- SchlumpfSoftControlsDemo/FormExplorerTreeView.Designer.vb
+++ SchlumpfSoftControlsDemo/FormExplorerTreeView.Designer.vb
@@ -22,76 +22,73 @@ Partial Class FormExplorerTreeView
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        Dim SplitContainer1 As System.Windows.Forms.SplitContainer
-        Dim SplitContainer2 As System.Windows.Forms.SplitContainer
-        Me.ExplorerTreeView1 = New SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.ExplorerTreeView = New SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
         Me.TextBox = New System.Windows.Forms.TextBox()
         Me.TableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
         Me.ListView = New System.Windows.Forms.ListView()
         Me.ButtonSearchPath = New System.Windows.Forms.Button()
         Me.LabelPath = New System.Windows.Forms.Label()
-        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
-        SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        SplitContainer2 = New System.Windows.Forms.SplitContainer()
-        CType(SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        SplitContainer1.Panel1.SuspendLayout()
-        SplitContainer1.Panel2.SuspendLayout()
-        SplitContainer1.SuspendLayout()
-        CType(SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
-        SplitContainer2.Panel1.SuspendLayout()
-        SplitContainer2.Panel2.SuspendLayout()
-        SplitContainer2.SuspendLayout()
+        Me.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
         Me.TableLayoutPanel.SuspendLayout()
         Me.SuspendLayout()
         '
         'SplitContainer1
         '
-        SplitContainer1.Location = New System.Drawing.Point(12, 12)
-        SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Location = New System.Drawing.Point(12, 12)
+        Me.SplitContainer1.Name = "SplitContainer1"
         '
         'SplitContainer1.Panel1
         '
-        SplitContainer1.Panel1.Controls.Add(Me.ExplorerTreeView1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.ExplorerTreeView)
         '
         'SplitContainer1.Panel2
         '
-        SplitContainer1.Panel2.Controls.Add(SplitContainer2)
-        SplitContainer1.Size = New System.Drawing.Size(639, 437)
-        SplitContainer1.SplitterDistance = 190
-        SplitContainer1.TabIndex = 2
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(639, 437)
+        Me.SplitContainer1.SplitterDistance = 190
+        Me.SplitContainer1.TabIndex = 2
         '
-        'ExplorerTreeView1
+        'ExplorerTreeView
         '
-        Me.ExplorerTreeView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ExplorerTreeView1.Indent = 19
-        Me.ExplorerTreeView1.ItemHeight = 16
-        Me.ExplorerTreeView1.LineColor = System.Drawing.Color.Black
-        Me.ExplorerTreeView1.Location = New System.Drawing.Point(0, 0)
-        Me.ExplorerTreeView1.Name = "ExplorerTreeView1"
-        Me.ExplorerTreeView1.ShowLines = True
-        Me.ExplorerTreeView1.ShowPlusMinus = True
-        Me.ExplorerTreeView1.ShowRootLines = True
-        Me.ExplorerTreeView1.Size = New System.Drawing.Size(190, 437)
-        Me.ExplorerTreeView1.TabIndex = 0
+        Me.ExplorerTreeView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ExplorerTreeView.Indent = 19
+        Me.ExplorerTreeView.ItemHeight = 16
+        Me.ExplorerTreeView.LineColor = System.Drawing.Color.Black
+        Me.ExplorerTreeView.Location = New System.Drawing.Point(0, 0)
+        Me.ExplorerTreeView.Name = "ExplorerTreeView"
+        Me.ExplorerTreeView.ShowLines = True
+        Me.ExplorerTreeView.ShowPlusMinus = True
+        Me.ExplorerTreeView.ShowRootLines = True
+        Me.ExplorerTreeView.Size = New System.Drawing.Size(190, 437)
+        Me.ExplorerTreeView.TabIndex = 0
         '
         'SplitContainer2
         '
-        SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
-        SplitContainer2.Location = New System.Drawing.Point(0, 0)
-        SplitContainer2.Name = "SplitContainer2"
-        SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
         '
         'SplitContainer2.Panel1
         '
-        SplitContainer2.Panel1.Controls.Add(Me.TextBox)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.TextBox)
         '
         'SplitContainer2.Panel2
         '
-        SplitContainer2.Panel2.Controls.Add(Me.TableLayoutPanel)
-        SplitContainer2.Size = New System.Drawing.Size(445, 437)
-        SplitContainer2.SplitterDistance = 118
-        SplitContainer2.TabIndex = 0
+        Me.SplitContainer2.Panel2.Controls.Add(Me.TableLayoutPanel)
+        Me.SplitContainer2.Size = New System.Drawing.Size(445, 437)
+        Me.SplitContainer2.SplitterDistance = 118
+        Me.SplitContainer2.TabIndex = 0
         '
         'TextBox
         '
@@ -164,30 +161,33 @@ Partial Class FormExplorerTreeView
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(663, 461)
-        Me.Controls.Add(SplitContainer1)
+        Me.Controls.Add(Me.SplitContainer1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FormExplorerTreeView"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "ExplorerTreeView Demo"
-        SplitContainer1.Panel1.ResumeLayout(False)
-        SplitContainer1.Panel2.ResumeLayout(False)
-        CType(SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        SplitContainer1.ResumeLayout(False)
-        SplitContainer2.Panel1.ResumeLayout(False)
-        SplitContainer2.Panel1.PerformLayout()
-        SplitContainer2.Panel2.ResumeLayout(False)
-        CType(SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
-        SplitContainer2.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.PerformLayout()
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
         Me.TableLayoutPanel.ResumeLayout(False)
         Me.ResumeLayout(False)
+
     End Sub
-    Private WithEvents ExplorerTreeView1 As SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView
+    Private WithEvents ExplorerTreeView As SchlumpfSoft.Controls.ExplorerTreeViewControl.ExplorerTreeView
     Private WithEvents TextBox As TextBox
     Private WithEvents TableLayoutPanel As TableLayoutPanel
     Private WithEvents ListView As ListView
     Private WithEvents ButtonSearchPath As Button
     Private WithEvents LabelPath As Label
-    Private WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Private WithEvents FolderBrowserDialog As FolderBrowserDialog
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
 End Class
--- SchlumpfSoftControlsDemo/FormExplorerTreeView.vb
+++ SchlumpfSoftControlsDemo/FormExplorerTreeView.vb
@@ -6,13 +6,15 @@
 
 Imports System.IO
 
+Imports SchlumpfSoft.Controls
+
 Public Class FormExplorerTreeView
 
     Public Sub New()
         Me.InitializeComponent()
     End Sub
 
-    Private Sub ExplorerTreeView1_SelectedPathChanged(sender As Object, e As SchlumpfSoft.Controls.ExplorerTreeViewControl.SelectedPathChangedEventArgs) Handles ExplorerTreeView1.SelectedPathChanged
+    Private Sub ExplorerTreeView_SelectedPathChanged(sender As Object, e As ExplorerTreeViewControl.SelectedPathChangedEventArgs) Handles ExplorerTreeView.SelectedPathChanged, ExplorerTreeView.SelectedPathChanged
         Dim selpath As String = e.SelectedPath ' aktuell ausgewählten Pfad anzeigen
         Dim text As String = $"{Me.TextBox.Text}{vbCrLf}Aktuell ausgewählter Pfad: {selpath}"
         Me.TextBox.Text = text
@@ -31,10 +33,10 @@ Public Class FormExplorerTreeView
     End Sub
 
     Private Sub ButtonSearchPath_Click(sender As Object, e As EventArgs) Handles ButtonSearchPath.Click
-        Dim result As DialogResult = Me.FolderBrowserDialog1.ShowDialog(Me)
+        Dim result As DialogResult = Me.FolderBrowserDialog.ShowDialog(Me)
         If result = DialogResult.OK Then
-            Me.LabelPath.Text = Me.FolderBrowserDialog1.SelectedPath
-            Dim unused = Me.ExplorerTreeView1.ExpandPath(Me.FolderBrowserDialog1.SelectedPath)
+            Me.LabelPath.Text = Me.FolderBrowserDialog.SelectedPath
+            Dim unused = Me.ExplorerTreeView.ExpandPath(Me.FolderBrowserDialog.SelectedPath)
         End If
     End Sub
 
