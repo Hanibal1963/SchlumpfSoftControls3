b105277b8c8211731ab5d4427d9e5e5a25a93355
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-08 19:40:14 +0200

    Import und Typreferenzen für NotifyFormControl angepasst
    Es wurde der Import von SchlumpfSoft.Controls am Anfang der Datei hinzugefügt.
    
    Die Typen für Style und Design werden nun direkt über NotifyFormControl.NotifyFormStyle bzw. NotifyFormControl.NotifyFormDesign referenziert, anstatt über den vollqualifizierten Namespace SchlumpfSoft.Controls.NotifyFormControl.

--- SchlumpfSoftControlsDemo/FormNotifyForm.vb
+++ SchlumpfSoftControlsDemo/FormNotifyForm.vb
@@ -4,6 +4,8 @@
 ' Datum: 06.09.2026
 ' --------------------------------------------------------------------------------------------------------
 
+Imports SchlumpfSoft.Controls
+
 Public Class FormNotifyForm
 
     Public Sub New()
@@ -19,8 +21,8 @@ Public Class FormNotifyForm
         Me.NotifyForm.Title = Me.TextBox_Title.Text
         Me.NotifyForm.Message = Me.TextBox_Message.Text
         Me.NotifyForm.ShowTime = CInt(Me.NumericUpDown_ShowTime.Value * 1000)
-        Me.NotifyForm.Style = CType(Me.ComboBox_Style.SelectedIndex, SchlumpfSoft.Controls.NotifyFormControl.NotifyFormStyle)
-        Me.NotifyForm.Design = CType(Me.ComboBox_Design.SelectedIndex, SchlumpfSoft.Controls.NotifyFormControl.NotifyFormDesign)
+        Me.NotifyForm.Style = CType(Me.ComboBox_Style.SelectedIndex, NotifyFormControl.NotifyFormStyle)
+        Me.NotifyForm.Design = CType(Me.ComboBox_Design.SelectedIndex, NotifyFormControl.NotifyFormDesign)
         Me.NotifyForm.Show()
     End Sub
 
