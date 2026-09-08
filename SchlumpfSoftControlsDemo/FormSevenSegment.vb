86980d0332ff20c3072f231ba3362ee5c3b91bcf
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-08 19:32:28 +0200

    FormSevenSegment um neue Steuerelemente erweitert
    Die FormSevenSegment wurde um SingleDigit, MultiDigit, zwei TextBoxen, zwei Labels sowie zwei Buttons zur Farbauswahl ergänzt. Die InitializeComponent-Methode wurde angepasst, um die neuen Controls zu initialisieren und anzuordnen. Die Fenstergröße wurde verkleinert und die Steuerelemente übersichtlich platziert. Event-Handler für Textänderungen und Button-Klicks wurden hinzugefügt (Buttons noch ohne Logik). Kommentare und XML-Dokumentation wurden ergänzt, inklusive Autoreninformation.

--- SchlumpfSoftControlsDemo/FormSevenSegment.vb
+++ SchlumpfSoftControlsDemo/FormSevenSegment.vb
@@ -1,3 +1,29 @@
-﻿Public Class FormSevenSegment
+﻿' --------------------------------------------------------------------------------------------------------
+' Datei: FormSevenSegment.vb
+' Author: Andreas Sauer
+' Datum: 08.09.2026
+' --------------------------------------------------------------------------------------------------------
+
+Public Class FormSevenSegment
+
+    Public Sub New()
+        Me.InitializeComponent()
+    End Sub
+
+    Private Sub TextBox_SingleDigit_TextChanged(sender As Object, e As EventArgs) Handles TextBox_SingleDigit.TextChanged
+        Me.SingleDigit.DigitValue = CType(sender, TextBox).Text
+    End Sub
+
+    Private Sub TextBox_MultiDigit_TextChanged(sender As Object, e As EventArgs) Handles TextBox_MultiDigit.TextChanged
+        Me.MultiDigit.Value = CType(sender, TextBox).Text
+    End Sub
+
+    Private Sub Button_InactiveColor_Click(sender As Object, e As EventArgs) Handles Button_InactiveColor.Click
+
+    End Sub
+
+    Private Sub Button_ForeColor_Click(sender As Object, e As EventArgs) Handles Button_ForeColor.Click
+
+    End Sub
 
 End Class
