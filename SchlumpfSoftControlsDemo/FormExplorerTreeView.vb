a0f2c6f14f7139fa9f003c32a0f7d06b52a01f20
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-08 19:33:03 +0200

    FormExplorerTreeView: Neues Layout & Ereignisse
    Die FormExplorerTreeView wurde grundlegend überarbeitet:
    - Zwei SplitContainer strukturieren das Layout mit ExplorerTreeView, TextBox, TableLayoutPanel, ListView, Button und Label.
    - Dynamische Initialisierung und Anordnung der Steuerelemente im Designer.
    - Fenstergröße angepasst.
    - Klasse im Code-Behind umstrukturiert, mit XML-Kommentaren und deutschen Inline-Kommentaren versehen.
    - Ereignisbehandlung: Auswahl im ExplorerTreeView zeigt Pfad in TextBox und listet Inhalte im ListView.
    - „Pfad suchen ...“-Button öffnet FolderBrowserDialog, setzt Pfad in Label und ExplorerTreeView.
    - Hilfsmethode GetName extrahiert Namen aus Pfaden.
    - Fehlerbehandlung für Zugriffsverletzungen und IO-Fehler ergänzt.

--- SchlumpfSoftControlsDemo/FormExplorerTreeView.vb
+++ SchlumpfSoftControlsDemo/FormExplorerTreeView.vb
@@ -1,2 +1,45 @@
-﻿Public Class FormExplorerTreeView
+﻿' --------------------------------------------------------------------------------------------------------
+' Datei: FormExplorerTreeView.vb
+' Author: Andreas Sauer
+' Datum: 08.09.2026
+' --------------------------------------------------------------------------------------------------------
+
+Imports System.IO
+
+Public Class FormExplorerTreeView
+
+    Public Sub New()
+        Me.InitializeComponent()
+    End Sub
+
+    Private Sub ExplorerTreeView1_SelectedPathChanged(sender As Object, e As SchlumpfSoft.Controls.ExplorerTreeViewControl.SelectedPathChangedEventArgs) Handles ExplorerTreeView1.SelectedPathChanged
+        Dim selpath As String = e.SelectedPath ' aktuell ausgewählten Pfad anzeigen
+        Dim text As String = $"{Me.TextBox.Text}{vbCrLf}Aktuell ausgewählter Pfad: {selpath}"
+        Me.TextBox.Text = text
+        Me.ListView.Items.Clear() ' Liste leeren
+        Try
+            For Each dir As String In Directory.GetDirectories(selpath)  ' Unterverzeichnisse auslesen
+                Dim unused1 = Me.ListView.Items.Add(Me.GetName(dir))
+            Next
+            For Each file As String In Directory.GetFiles(selpath) ' Dateien auslesen
+                Dim unused2 = Me.ListView.Items.Add(Me.GetName(file))
+            Next
+        Catch ex As UnauthorizedAccessException
+            ' geschützter Ordner: ignorieren oder Hinweis anzeigen
+        Catch ex As IOException
+        End Try
+    End Sub
+
+    Private Sub ButtonSearchPath_Click(sender As Object, e As EventArgs) Handles ButtonSearchPath.Click
+        Dim result As DialogResult = Me.FolderBrowserDialog1.ShowDialog(Me)
+        If result = DialogResult.OK Then
+            Me.LabelPath.Text = Me.FolderBrowserDialog1.SelectedPath
+            Dim unused = Me.ExplorerTreeView1.ExpandPath(Me.FolderBrowserDialog1.SelectedPath)
+        End If
+    End Sub
+
+    Private Function GetName(FileOrDirectory As String) As String
+        Return Path.GetFileName(FileOrDirectory)
+    End Function
+
 End Class
