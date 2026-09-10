1f4d696cf65c981061158815c0d74d8561a6f29d
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-10 19:50:22 +0200

    Neue Steuerelemente & Events für ColorProgressBar-Form
    In der Datei FormColorProgressBar.Designer.vb wurden mehrere neue Steuerelemente (NumericUpDown, CheckBoxen, Buttons, Label, ColorDialog, ColorProgressBar) hinzugefügt, im Layout platziert und als WithEvents deklariert. Die Formgröße wurde angepasst und die Controls der Form hinzugefügt.
    
    In FormColorProgressBar.resx wurde die TrayLocation für das ColorDialog-Steuerelement als Metadaten eingetragen.
    
    In FormColorProgressBar.vb wurden Initialisierungen und Event-Handler für die neuen Steuerelemente implementiert: Synchronisierung der Werte zwischen NumericUpDown und ColorProgressBar, Umschalten von Glanz und Rahmenanzeige, Farbänderungen für Balken, Rahmen und Hintergrund über Buttons und ColorDialog sowie ein Klick-Event für die ColorProgressBar mit Debug-Ausgabe.

--- SchlumpfSoftControlsDemo/FormColorProgressBar.vb
+++ SchlumpfSoftControlsDemo/FormColorProgressBar.vb
@@ -10,6 +10,51 @@ Public Class FormColorProgressBar
 
     Public Sub New()
         Me.InitializeComponent()
+        Me.ColorProgressBar.Value = CInt(Me.NumericUpDown_ProgressValue.Value)
+        Me.ColorProgressBar.ProgressMaximumValue = CInt(Me.NumericUpDown_ProgressValue.Maximum)
+        Me.CheckBox_ShowGliss.Checked = Me.ColorProgressBar.IsGlossy
+        Me.CheckBox_ShowBorder.Checked = Me.ColorProgressBar.ShowBorder
+    End Sub
+
+    Private Sub CheckBox_ShowGliss_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox_ShowGliss.CheckedChanged
+        Me.ColorProgressBar.IsGlossy = CType(sender, CheckBox).Checked
+    End Sub
+
+    Private Sub CheckBox_ShowBorder_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox_ShowBorder.CheckedChanged
+        Me.ColorProgressBar.ShowBorder = CType(sender, CheckBox).Checked
+    End Sub
+
+    Private Sub NumericUpDown_ProgressValue_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown_ProgressValue.ValueChanged
+        Me.ColorProgressBar.Value = CInt(CType(sender, NumericUpDown).Value)
+    End Sub
+
+    Private Sub Button_BarColorChange_Click(sender As Object, e As EventArgs) Handles Button_BarColorChange.Click
+        Me.ColorDialog.Color = Me.ColorProgressBar.BarColor
+        If Me.ColorDialog.ShowDialog(Me) = DialogResult.OK Then
+            Me.ColorProgressBar.BarColor = Me.ColorDialog.Color
+        End If
+    End Sub
+
+    Private Sub Button_BorderColorChange_Click(sender As Object, e As EventArgs) Handles Button_BorderColorChange.Click
+        Me.ColorDialog.Color = Me.ColorProgressBar.BorderColor
+        If Me.ColorDialog.ShowDialog(Me) = DialogResult.OK Then
+            Me.ColorProgressBar.BorderColor = Me.ColorDialog.Color
+        End If
+    End Sub
+
+    Private Sub Button_EmptyColorChange_Click(sender As Object, e As EventArgs) Handles Button_EmptyColorChange.Click
+        Me.ColorDialog.Color = Me.ColorProgressBar.EmptyColor
+        If Me.ColorDialog.ShowDialog(Me) = DialogResult.OK Then
+            Me.ColorProgressBar.EmptyColor = Me.ColorDialog.Color
+        End If
+    End Sub
+
+    Private Sub ColorProgressBar_Click(sender As Object, e As EventArgs) Handles ColorProgressBar.Click
+
+#If DEBUG Then
+        Debug.Print($"Es wurde auf {CType(sender, ColorProgressBarControl.ColorProgressBar).Name} geklickt.")
+#End If
+
     End Sub
 
 End Class
