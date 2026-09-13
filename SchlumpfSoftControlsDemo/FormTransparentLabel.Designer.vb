0100d470a8fdecca6613a97ce5deb400e0f1308e
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2026-09-13 09:44:47 +0200

    Formularname und Designer-Steuerelemente korrigiert
    In FormTransparentLabel.Designer.vb wurde der Klassenname auf FormTransparentLabel berichtigt. Zwei neue Steuerelemente (PictureBox, TransparentLabel) wurden hinzugefügt und im Designer konfiguriert. Die Controls werden nun korrekt im Formular platziert und initialisiert. ClientSize und Formularname wurden angepasst. In AssemblyInfo.vb wurde die AssemblyVersion und AssemblyFileVersion auf 1.2026.0913.0 erhöht.

--- SchlumpfSoftControlsDemo/FormTransparentLabel.Designer.vb
+++ SchlumpfSoftControlsDemo/FormTransparentLabel.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class FormTransparentlabel
+Partial Class FormTransparentLabel
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
@@ -22,20 +22,46 @@ Partial Class FormTransparentlabel
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.TransparentLabel = New SchlumpfSoft.Controls.TransparentLabelControl.TransparentLabel()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'FormTransparentlabel
+        'PictureBox
+        '
+        Me.PictureBox.Image = Global.SchlumpfSoft.My.Resources.Resources.PapaSchlumpf1
+        Me.PictureBox.Location = New System.Drawing.Point(44, 52)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(138, 140)
+        Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox.TabIndex = 1
+        Me.PictureBox.TabStop = False
+        '
+        'TransparentLabel
+        '
+        Me.TransparentLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TransparentLabel.Location = New System.Drawing.Point(28, 26)
+        Me.TransparentLabel.Name = "TransparentLabel"
+        Me.TransparentLabel.Size = New System.Drawing.Size(213, 181)
+        Me.TransparentLabel.TabIndex = 2
+        Me.TransparentLabel.Text = "Dieser Text überlagert das" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "daunterliegende Bild" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Test" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Test" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Test"
+        '
+        'FormTransparentLabel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(538, 349)
+        Me.ClientSize = New System.Drawing.Size(424, 241)
+        Me.Controls.Add(Me.TransparentLabel)
+        Me.Controls.Add(Me.PictureBox)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "FormTransparentlabel"
+        Me.Name = "FormTransparentLabel"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "TransparentLabel Demo"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-
     End Sub
+    Private WithEvents PictureBox As PictureBox
+    Friend WithEvents TransparentLabel As SchlumpfSoft.Controls.TransparentLabelControl.TransparentLabel
 End Class
